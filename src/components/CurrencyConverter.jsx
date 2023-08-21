934f585019db0c34e7d55f1ac12ca3db7a7faed4
Nhien <nhienau@gmail.com>
2023-08-21 14:47:13 +0700

    Implement rate fetching

--- src/components/CurrencyConverter.jsx
+++ src/components/CurrencyConverter.jsx
@@ -3,17 +3,64 @@ import styles from "./CurrencyConverter.module.css";
 import CurrencyInput from "./CurrencyInput";
 import SwapHorizIcon from "@mui/icons-material/SwapHoriz";
 
+function toDateString(timestamp) {
+  return new Intl.DateTimeFormat(navigator.language, {
+    dateStyle: "medium",
+  }).format(new Date(timestamp));
+}
+
 function CurrencyConverter({ symbols }) {
   const [fromAmount, setFromAmount] = useState("");
   const [fromCurrency, setFromCurrency] = useState(null);
   const [toAmount, setToAmount] = useState("");
   const [toCurrency, setToCurrency] = useState(null);
+  const [exchangeRate, setExchangeRate] = useState(null);
 
   function handleSwap() {
     if (fromCurrency === null || toCurrency === null) return;
     setFromCurrency(toCurrency);
     setToCurrency(fromCurrency);
   }
+
+  useEffect(
+    function () {
+      if (fromCurrency === null || toCurrency === null) return;
+      async function fetchExchangeRate() {
+        try {
+          setExchangeRate(null);
+
+          const myHeaders = new Headers();
+          myHeaders.append("apikey", import.meta.env.VITE_API_KEY);
+
+          const requestOptions = {
+            method: "GET",
+            redirect: "follow",
+            headers: myHeaders,
+            withCredentials: true, // in order to work fine in Firefox
+          };
+
+          const response = await fetch(
+            `${import.meta.env.VITE_API_URL}/latest?symbols=${
+              toCurrency.value
+            }&base=${fromCurrency.value}`,
+            requestOptions
+          );
+          const data = await response.json();
+          const result = {
+            fromCur: data.base,
+            toCur: Object.keys(data.rates)[0],
+            rate: Object.values(data.rates)[0],
+            timestamp: data.timestamp * 1000,
+          };
+          setExchangeRate(result);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+      fetchExchangeRate();
+    },
+    [fromCurrency, toCurrency]
+  );
 
   return (
     <section className={styles.converter}>
@@ -34,9 +81,12 @@ function CurrencyConverter({ symbols }) {
           setCurrency={setToCurrency}
         />
       </div>
-      <summary className={styles["rate-summary"]}>
-        X USD ~= Y EUR &bull; Aug 20, 2023
-      </summary>
+      {exchangeRate && (
+        <summary className={styles["rate-summary"]}>
+          1 {fromCurrency.value} &asymp; {exchangeRate.rate} {toCurrency.value}{" "}
+          &bull; {toDateString(exchangeRate.timestamp)}
+        </summary>
+      )}
     </section>
   );
 }
